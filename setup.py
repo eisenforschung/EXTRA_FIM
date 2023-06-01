f41a72100284b7f45a73842bb288d195f5fac852
Bhatt993 <84179891+Bhatt993@users.noreply.github.com>
2023-06-01 10:18:15 +0200

    Create setup.py
    dictionary

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+''' an input dictionary to set up the initial parameters
+'''
+
+Simulator = {
+            'working_directory': '/u/cfrey/calcs/Ni-FIM/Ni012/pureNi/wv',
+            'ik': 0,
+            'z_max': 35,
+            'izstart_min': 90,
+            'izend': 65,
+            'cutoff': 10,
+            'limit': 1e-10,
+            'E_fermi': 40.4464 ,
+            'E_max': 47.8888,
+            'ionization_energies': [10., 15., 21.5]
+    }
